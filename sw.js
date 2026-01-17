4d8c15de7889fccd8a227d84236c97ac2140164e
Samarth Patodekar <s7samarth@gmail.com>
2026-01-17 15:14:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,12 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated');
-});
-
 self.addEventListener('notificationclick', (event) => {
+    // When you click the notification, it clears the badge (resets to 0)
     event.notification.close();
+    if (navigator.clearAppBadge) {
+        navigator.clearAppBadge();
+    }
     event.waitUntil(
         clients.matchAll({ type: 'window' }).then((clientList) => {
-            if (clientList.length > 0) {
-                return clientList[0].focus();
-            }
+            if (clientList.length > 0) return clientList[0].focus();
             return clients.openWindow('/');
         })
     );
