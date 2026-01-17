078f46e59116c4056f882b99a798d61eb2364cc9
Samarth Patodekar <s7samarth@gmail.com>
2026-01-17 15:01:00 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activated');
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then((clientList) => {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
