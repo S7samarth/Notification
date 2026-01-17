c081a3661109cbf91706e6165ce3ea8a0f5a0d93
Samarth Patodekar <s7samarth@gmail.com>
2026-01-17 16:09:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,22 +3,12 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-    // Take control of the page immediately to enable badges
     event.waitUntil(clients.claim());
 });
 
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
-    
-    // Clear the badge when the user clicks the notification
     if (navigator.clearAppBadge) {
         navigator.clearAppBadge();
     }
-
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then((clientList) => {
-            if (clientList.length > 0) return clientList[0].focus();
-            return clients.openWindow('./');
-        })
-    );
 });
